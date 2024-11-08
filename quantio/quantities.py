b44f6485aa3a31c02d2acf8ed9e123254df87f9d
unexcellent <>
2024-11-08 09:38:43 +0100

    feat: add common metric units to Length

--- quantio/quantities.py
+++ quantio/quantities.py
@@ -1,13 +1,51 @@
+from __future__ import annotations
+
+from typing import ClassVar
+
+
 class Length:
     """The one-dimensional extent of an object or the distance between two points."""
 
     _meters: float
+    UNIT_CONVERSION: ClassVar[dict[str, float]] = {
+        "kilometers": 10**3,
+        "meters": 10**0,
+        "centimeters": 10**-2,
+        "millimeters": 10**-3,
+        "micrometers": 10**-6,
+    }
 
-    def __init__(self, meters: float = 0.0) -> None:
+    def __init__(self, **kwargs: float) -> None:
         """Construct this class with the used units."""
-        self._meters = meters
+        self._meters = (
+            kwargs.get("kilometers", 0.0) * self.UNIT_CONVERSION["kilometers"]
+            + kwargs.get("meters", 0.0) * self.UNIT_CONVERSION["meters"]
+            + kwargs.get("centimeters", 0.0) * self.UNIT_CONVERSION["centimeters"]
+            + kwargs.get("millimeters", 0.0) * self.UNIT_CONVERSION["millimeters"]
+            + kwargs.get("micrometers", 0.0) * self.UNIT_CONVERSION["micrometers"]
+        )
+
+    @property
+    def kilometers(self) -> float:
+        """The length in kilometers."""
+        return self._meters / self.UNIT_CONVERSION["kilometers"]
 
     @property
     def meters(self) -> float:
         """The length in meters."""
-        return self._meters
+        return self._meters / self.UNIT_CONVERSION["meters"]
+
+    @property
+    def centimeters(self) -> float:
+        """The length in centimeters."""
+        return self._meters / self.UNIT_CONVERSION["centimeters"]
+
+    @property
+    def millimeters(self) -> float:
+        """The length in millimeters."""
+        return self._meters / self.UNIT_CONVERSION["millimeters"]
+
+    @property
+    def micrometers(self) -> float:
+        """The length in micrometers."""
+        return self._meters / self.UNIT_CONVERSION["micrometers"]
--- test/test_length.py
+++ test/test_length.py
@@ -3,9 +3,34 @@ import pytest
 from quantio import Length
 
 
-def test_construction():
-    actual = Length(meters=5)
-    assert actual.meters == 5
+def test_construction__multiple_units():
+    actual = Length(meters=5, kilometers=1, centimeters=7)
+    assert actual.meters == 1005.07
+
+
+def test_kilometers():
+    actual = Length(meters=1)
+    assert actual.kilometers == 10**-3
+
+
+def test_meters():
+    actual = Length(meters=1)
+    assert actual.meters == 1
+
+
+def test_centimeters():
+    actual = Length(meters=1)
+    assert actual.centimeters == 10**2
+
+
+def test_millimeters():
+    actual = Length(meters=1)
+    assert actual.millimeters == 10**3
+
+
+def test_micrometers():
+    actual = Length(meters=1)
+    assert actual.micrometers == 10**6
 
 
 if __name__ == "__main__":
