d2788cbdc324e9f0f838cb723293715c27637ae0
unexcellent <>
2024-11-08 14:03:27 +0100

    test: extract tests associated with base class

--- test/test_length.py
+++ test/test_length.py
@@ -3,11 +3,6 @@ import pytest
 from quantio import Length, CanNotAddTypesError, CanNotSubtractTypesError
 
 
-def test_construction__multiple_units():
-    actual = Length(meters=5, kilometers=1, centimeters=7)
-    assert actual.meters == 1005.07
-
-
 def test_miles():
     actual = Length(meters=1)
     assert actual.miles == 1 / 1609.34
@@ -46,36 +41,6 @@ def test_millimeters():
 def test_micrometers():
     actual = Length(micrometers=1)
     assert actual.micrometers == 1
-
-
-def test_add__success():
-    length1 = Length(meters=1)
-    length2 = Length(meters=2)
-
-    actual = length1 + length2
-    assert actual == Length(meters=3)
-
-
-def test_add__false_class():
-    length = Length(meters=1)
-
-    with pytest.raises(CanNotAddTypesError):
-        length += 1
-
-
-def test_sub__success():
-    length1 = Length(meters=1)
-    length2 = Length(meters=2)
-
-    actual = length2 - length1
-    assert actual == Length(meters=1)
-
-
-def test_sub__false_class():
-    length = Length(meters=1)
-
-    with pytest.raises(CanNotSubtractTypesError):
-        length -= 1
 
 
 if __name__ == "__main__":
