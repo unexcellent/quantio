e8b58126f49f4788fd64356006dfb6fba9f69504
unexcellent <>
2024-11-19 07:42:27 +0100

    fix: add Frequency to root package

--- quantio/__init__.py
+++ quantio/__init__.py
@@ -1,13 +1,14 @@
 """The main quantio package."""
 
 from .exceptions import CanNotAddTypesError, CanNotSubtractTypesError, NoUnitSpecifiedError
-from .quantities import Acceleration, Angle, Area, Length, Mass, Time, Velocity
+from .quantities import Acceleration, Angle, Area, Frequency, Length, Mass, Time, Velocity
 from .vector import Vector
 
 __all__ = [
     "Acceleration",
     "Angle",
     "Area",
+    "Frequency",
     "Length",
     "Mass",
     "Time",
