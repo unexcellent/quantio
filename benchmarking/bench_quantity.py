cefc05be3a222d8d8e788e0b2db50da6d0055ff4
unexcellent <>
2024-12-11 12:46:03 +0100

    test: implement basic benchmarking

---
+++ benchmarking/bench_quantity.py
@@ -0,0 +1,15 @@
+import pytest
+
+from quantio import Time
+
+
+def init_only_base_unit():
+    Time(seconds=1)
+
+
+def bench_init(benchmark):
+    benchmark.pedantic(init_only_base_unit, iterations=100, rounds=100)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
