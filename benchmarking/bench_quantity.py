73263772ca4a811be5e448c45f3b3dbd4d760723
unexcellent <>
2024-12-11 14:43:34 +0100

    test: refactor Quantitiy init bench

--- benchmarking/bench_quantity.py
+++ benchmarking/bench_quantity.py
@@ -3,12 +3,9 @@ import pytest
 from quantio import Time
 
 
-def init_only_base_unit():
-    Time(seconds=1, milliseconds=1)
-
-
 def bench_init(benchmark):
-    benchmark.pedantic(init_only_base_unit, iterations=100, rounds=100)
+    f = lambda: Time(seconds=1, milliseconds=1)
+    benchmark.pedantic(f, iterations=100, rounds=100)
 
 
 if __name__ == "__main__":
